112fa60e0c342c57edb1bc0f701745c059a93683
patricio.toledo <patricio.ezequiel.toledo@gmail.com>
2023-06-16 18:29:25 -0300

    New levels flags

--- src/components/LevelBanderas/index.js
+++ src/components/LevelBanderas/index.js
@@ -25,6 +25,81 @@ import {
   Banderas23,
   Banderas24,
   Banderas25,
+  Banderas26,
+  Banderas27,
+  Banderas28,
+  Banderas29,
+  Banderas30,
+  Banderas31,
+  Banderas32,
+  Banderas33,
+  Banderas34,
+  Banderas35,
+  Banderas36,
+  Banderas37,
+  Banderas38,
+  Banderas39,
+  Banderas40,
+  Banderas41,
+  Banderas42,
+  Banderas43,
+  Banderas44,
+  Banderas45,
+  Banderas46,
+  Banderas47,
+  Banderas48,
+  Banderas49,
+  Banderas50,
+  Banderas51,
+  Banderas52,
+  Banderas53,
+  Banderas54,
+  Banderas55,
+  Banderas56,
+  Banderas57,
+  Banderas58,
+  Banderas59,
+  Banderas60,
+  Banderas61,
+  Banderas62,
+  Banderas63,
+  Banderas64,
+  Banderas65,
+  Banderas66,
+  Banderas67,
+  Banderas68,
+  Banderas69,
+  Banderas70,
+  Banderas71,
+  Banderas72,
+  Banderas73,
+  Banderas74,
+  Banderas75,
+  Banderas76,
+  Banderas77,
+  Banderas78,
+  Banderas79,
+  Banderas80,
+  Banderas81,
+  Banderas82,
+  Banderas83,
+  Banderas84,
+  Banderas85,
+  Banderas86,
+  Banderas87,
+  Banderas88,
+  Banderas89,
+  Banderas90,
+  Banderas91,
+  Banderas92,
+  Banderas93,
+  Banderas94,
+  Banderas95,
+  Banderas96,
+  Banderas97,
+  Banderas98,
+  Banderas99,
+  Banderas100,
 } from '../SVG/Banderas'
 
 const LevelBanderas = ({ level }) => {
@@ -79,8 +154,158 @@ const LevelBanderas = ({ level }) => {
       return <Banderas24 />
     case 25:
       return <Banderas25 />
+    case 26:
+      return <Banderas26 />
+    case 27:
+      return <Banderas27 />
+    case 28:
+      return <Banderas28 />
+    case 29:
+      return <Banderas29 />
+    case 30:
+      return <Banderas30 />
+    case 31:
+      return <Banderas31 />
+    case 32:
+      return <Banderas32 />
+    case 33:
+      return <Banderas33 />
+    case 34:
+      return <Banderas34 />
+    case 35:
+      return <Banderas35 />
+    case 36:
+      return <Banderas36 />
+    case 37:
+      return <Banderas37 />
+    case 38:
+      return <Banderas38 />
+    case 39:
+      return <Banderas39 />
+    case 40:
+      return <Banderas40 />
+    case 41:
+      return <Banderas41 />
+    case 42:
+      return <Banderas42 />
+    case 43:
+      return <Banderas43 />
+    case 44:
+      return <Banderas44 />
+    case 45:
+      return <Banderas45 />
+    case 46:
+      return <Banderas46 />
+    case 47:
+      return <Banderas47 />
+    case 48:
+      return <Banderas48 />
+    case 49:
+      return <Banderas49 />
+    case 50:
+      return <Banderas50 />
+    case 51:
+      return <Banderas51 />
+    case 52:
+      return <Banderas52 />
+    case 53:
+      return <Banderas53 />
+    case 54:
+      return <Banderas54 />
+    case 55:
+      return <Banderas55 />
+    case 56:
+      return <Banderas56 />
+    case 57:
+      return <Banderas57 />
+    case 58:
+      return <Banderas58 />
+    case 59:
+      return <Banderas59 />
+    case 60:
+      return <Banderas60 />
+    case 61:
+      return <Banderas61 />
+    case 62:
+      return <Banderas62 />
+    case 63:
+      return <Banderas63 />
+    case 64:
+      return <Banderas64 />
+    case 65:
+      return <Banderas65 />
+    case 66:
+      return <Banderas66 />
+    case 67:
+      return <Banderas67 />
+    case 68:
+      return <Banderas68 />
+    case 69:
+      return <Banderas69 />
+    case 70:
+      return <Banderas70 />
+    case 71:
+      return <Banderas71 />
+    case 72:
+      return <Banderas72 />
+    case 73:
+      return <Banderas73 />
+    case 74:
+      return <Banderas74 />
+    case 75:
+      return <Banderas75 />
+    case 76:
+      return <Banderas76 />
+    case 77:
+      return <Banderas77 />
+    case 78:
+      return <Banderas78 />
+    case 79:
+      return <Banderas79 />
+    case 80:
+      return <Banderas80 />
+    case 81:
+      return <Banderas81 />
+    case 82:
+      return <Banderas82 />
+    case 83:
+      return <Banderas83 />
+    case 84:
+      return <Banderas84 />
+    case 85:
+      return <Banderas85 />
+    case 86:
+      return <Banderas86 />
+    case 87:
+      return <Banderas87 />
+    case 88:
+      return <Banderas88 />
+    case 89:
+      return <Banderas89 />
+    case 90:
+      return <Banderas90 />
+    case 91:
+      return <Banderas91 />
+    case 92:
+      return <Banderas92 />
+    case 93:
+      return <Banderas93 />
+    case 94:
+      return <Banderas94 />
+    case 95:
+      return <Banderas95 />
+    case 96:
+      return <Banderas96 />
+    case 97:
+      return <Banderas97 />
+    case 98:
+      return <Banderas98 />
+    case 99:
+      return <Banderas99 />
+    case 100:
+      return <Banderas100 />
     default:
-      return <Banderas1 />
+      return <Banderas100 />
   }
 }
 
