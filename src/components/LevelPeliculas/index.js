acbd2c1a365e0145fcb8ccfa5aaf54c9a4dbb258
patricio.toledo <patricio.ezequiel.toledo@gmail.com>
2023-07-08 23:06:32 -0300

    Levels movies

--- src/components/LevelPeliculas/index.js
+++ src/components/LevelPeliculas/index.js
@@ -25,6 +25,165 @@ import {
   Peliculas23,
   Peliculas24,
   Peliculas25,
+  Peliculas26,
+  Peliculas27,
+  Peliculas28,
+  Peliculas29,
+  Peliculas30,
+  Peliculas31,
+  Peliculas32,
+  Peliculas33,
+  Peliculas34,
+  Peliculas35,
+  Peliculas36,
+  Peliculas37,
+  Peliculas38,
+  Peliculas39,
+  Peliculas40,
+  Peliculas41,
+  Peliculas42,
+  Peliculas43,
+  Peliculas44,
+  Peliculas45,
+  Peliculas46,
+  Peliculas47,
+  Peliculas48,
+  Peliculas49,
+  Peliculas50,
+  Peliculas51,
+  Peliculas52,
+  Peliculas53,
+  Peliculas54,
+  Peliculas55,
+  Peliculas56,
+  Peliculas57,
+  Peliculas58,
+  Peliculas59,
+  Peliculas60,
+  Peliculas61,
+  Peliculas62,
+  Peliculas63,
+  Peliculas64,
+  Peliculas65,
+  Peliculas66,
+  Peliculas67,
+  Peliculas68,
+  Peliculas69,
+  Peliculas70,
+  Peliculas71,
+  Peliculas72,
+  Peliculas73,
+  Peliculas74,
+  Peliculas75,
+  Peliculas76,
+  Peliculas77,
+  Peliculas78,
+  Peliculas79,
+  Peliculas80,
+  Peliculas81,
+  Peliculas82,
+  Peliculas83,
+  Peliculas84,
+  Peliculas85,
+  Peliculas86,
+  Peliculas87,
+  Peliculas88,
+  Peliculas89,
+  Peliculas90,
+  Peliculas91,
+  Peliculas92,
+  Peliculas93,
+  Peliculas94,
+  Peliculas95,
+  Peliculas96,
+  Peliculas97,
+  Peliculas98,
+  Peliculas99,
+  Peliculas100,
+  Peliculas101,
+  Peliculas102,
+  Peliculas103,
+  Peliculas104,
+  Peliculas105,
+  Peliculas106,
+  Peliculas107,
+  Peliculas108,
+  Peliculas109,
+  Peliculas110,
+  Peliculas111,
+  Peliculas112,
+  Peliculas113,
+  Peliculas114,
+  Peliculas115,
+  Peliculas116,
+  Peliculas117,
+  Peliculas118,
+  Peliculas119,
+  Peliculas120,
+  Peliculas121,
+  Peliculas122,
+  Peliculas123,
+  Peliculas124,
+  Peliculas125,
+  Peliculas126,
+  Peliculas127,
+  Peliculas128,
+  Peliculas129,
+  Peliculas130,
+  Peliculas131,
+  Peliculas132,
+  Peliculas133,
+  Peliculas134,
+  Peliculas135,
+  Peliculas136,
+  Peliculas137,
+  Peliculas138,
+  Peliculas139,
+  Peliculas140,
+  Peliculas141,
+  Peliculas142,
+  Peliculas143,
+  Peliculas144,
+  Peliculas145,
+  Peliculas146,
+  Peliculas147,
+  Peliculas148,
+  Peliculas149,
+  Peliculas150,
+  Peliculas151,
+  Peliculas152,
+  Peliculas153,
+  Peliculas154,
+  Peliculas155,
+  Peliculas156,
+  Peliculas157,
+  Peliculas158,
+  Peliculas159,
+  Peliculas160,
+  Peliculas161,
+  Peliculas162,
+  Peliculas163,
+  Peliculas164,
+  Peliculas165,
+  Peliculas166,
+  Peliculas167,
+  Peliculas168,
+  Peliculas169,
+  Peliculas170,
+  Peliculas171,
+  Peliculas172,
+  Peliculas173,
+  Peliculas174,
+  Peliculas175,
+  Peliculas176,
+  Peliculas177,
+  Peliculas178,
+  Peliculas179,
+  Peliculas180,
+  Peliculas181,
+  Peliculas182,
+  Peliculas183,
+  Peliculas184,
 } from '../SVG/Peliculas'
 
 const LevelPeliculas = ({ level }) => {
@@ -79,8 +238,326 @@ const LevelPeliculas = ({ level }) => {
       return <Peliculas24 />
     case 25:
       return <Peliculas25 />
+    case 26:
+      return <Peliculas26 />
+    case 27:
+      return <Peliculas27 />
+    case 28:
+      return <Peliculas28 />
+    case 29:
+      return <Peliculas29 />
+    case 30:
+      return <Peliculas30 />
+    case 31:
+      return <Peliculas31 />
+    case 32:
+      return <Peliculas32 />
+    case 33:
+      return <Peliculas33 />
+    case 34:
+      return <Peliculas34 />
+    case 35:
+      return <Peliculas35 />
+    case 36:
+      return <Peliculas36 />
+    case 37:
+      return <Peliculas37 />
+    case 38:
+      return <Peliculas38 />
+    case 39:
+      return <Peliculas39 />
+    case 40:
+      return <Peliculas40 />
+    case 41:
+      return <Peliculas41 />
+    case 42:
+      return <Peliculas42 />
+    case 43:
+      return <Peliculas43 />
+    case 44:
+      return <Peliculas44 />
+    case 45:
+      return <Peliculas45 />
+    case 46:
+      return <Peliculas46 />
+    case 47:
+      return <Peliculas47 />
+    case 48:
+      return <Peliculas48 />
+    case 49:
+      return <Peliculas49 />
+    case 50:
+      return <Peliculas50 />
+    case 51:
+      return <Peliculas51 />
+    case 52:
+      return <Peliculas52 />
+    case 53:
+      return <Peliculas53 />
+    case 54:
+      return <Peliculas54 />
+    case 55:
+      return <Peliculas55 />
+    case 56:
+      return <Peliculas56 />
+    case 57:
+      return <Peliculas57 />
+    case 58:
+      return <Peliculas58 />
+    case 59:
+      return <Peliculas59 />
+    case 60:
+      return <Peliculas60 />
+    case 61:
+      return <Peliculas61 />
+    case 62:
+      return <Peliculas62 />
+    case 63:
+      return <Peliculas63 />
+    case 64:
+      return <Peliculas64 />
+    case 65:
+      return <Peliculas65 />
+    case 66:
+      return <Peliculas66 />
+    case 67:
+      return <Peliculas67 />
+    case 68:
+      return <Peliculas68 />
+    case 69:
+      return <Peliculas69 />
+    case 70:
+      return <Peliculas70 />
+    case 71:
+      return <Peliculas71 />
+    case 72:
+      return <Peliculas72 />
+    case 73:
+      return <Peliculas73 />
+    case 74:
+      return <Peliculas74 />
+    case 75:
+      return <Peliculas75 />
+    case 76:
+      return <Peliculas76 />
+    case 77:
+      return <Peliculas77 />
+    case 78:
+      return <Peliculas78 />
+    case 79:
+      return <Peliculas79 />
+    case 80:
+      return <Peliculas80 />
+    case 81:
+      return <Peliculas81 />
+    case 82:
+      return <Peliculas82 />
+    case 83:
+      return <Peliculas83 />
+    case 84:
+      return <Peliculas84 />
+    case 85:
+      return <Peliculas85 />
+    case 86:
+      return <Peliculas86 />
+    case 87:
+      return <Peliculas87 />
+    case 88:
+      return <Peliculas88 />
+    case 89:
+      return <Peliculas89 />
+    case 90:
+      return <Peliculas90 />
+    case 91:
+      return <Peliculas91 />
+    case 92:
+      return <Peliculas92 />
+    case 93:
+      return <Peliculas93 />
+    case 94:
+      return <Peliculas94 />
+    case 95:
+      return <Peliculas95 />
+    case 96:
+      return <Peliculas96 />
+    case 97:
+      return <Peliculas97 />
+    case 98:
+      return <Peliculas98 />
+    case 99:
+      return <Peliculas99 />
+    case 100:
+      return <Peliculas100 />
+    case 101:
+      return <Peliculas101 />
+    case 102:
+      return <Peliculas102 />
+    case 103:
+      return <Peliculas103 />
+    case 104:
+      return <Peliculas104 />
+    case 105:
+      return <Peliculas105 />
+    case 106:
+      return <Peliculas106 />
+    case 107:
+      return <Peliculas107 />
+    case 108:
+      return <Peliculas108 />
+    case 109:
+      return <Peliculas109 />
+    case 110:
+      return <Peliculas110 />
+    case 111:
+      return <Peliculas111 />
+    case 112:
+      return <Peliculas112 />
+    case 113:
+      return <Peliculas113 />
+    case 114:
+      return <Peliculas114 />
+    case 115:
+      return <Peliculas115 />
+    case 116:
+      return <Peliculas116 />
+    case 117:
+      return <Peliculas117 />
+    case 118:
+      return <Peliculas118 />
+    case 119:
+      return <Peliculas119 />
+    case 120:
+      return <Peliculas120 />
+    case 121:
+      return <Peliculas121 />
+    case 122:
+      return <Peliculas122 />
+    case 123:
+      return <Peliculas123 />
+    case 124:
+      return <Peliculas124 />
+    case 125:
+      return <Peliculas125 />
+    case 126:
+      return <Peliculas126 />
+    case 127:
+      return <Peliculas127 />
+    case 128:
+      return <Peliculas128 />
+    case 129:
+      return <Peliculas129 />
+    case 130:
+      return <Peliculas130 />
+    case 131:
+      return <Peliculas131 />
+    case 132:
+      return <Peliculas132 />
+    case 133:
+      return <Peliculas133 />
+    case 134:
+      return <Peliculas134 />
+    case 135:
+      return <Peliculas135 />
+    case 136:
+      return <Peliculas136 />
+    case 137:
+      return <Peliculas137 />
+    case 138:
+      return <Peliculas138 />
+    case 139:
+      return <Peliculas139 />
+    case 140:
+      return <Peliculas140 />
+    case 141:
+      return <Peliculas141 />
+    case 142:
+      return <Peliculas142 />
+    case 143:
+      return <Peliculas143 />
+    case 144:
+      return <Peliculas144 />
+    case 145:
+      return <Peliculas145 />
+    case 146:
+      return <Peliculas146 />
+    case 147:
+      return <Peliculas147 />
+    case 148:
+      return <Peliculas148 />
+    case 149:
+      return <Peliculas149 />
+    case 150:
+      return <Peliculas150 />
+    case 151:
+      return <Peliculas151 />
+    case 152:
+      return <Peliculas152 />
+    case 153:
+      return <Peliculas153 />
+    case 154:
+      return <Peliculas154 />
+    case 155:
+      return <Peliculas155 />
+    case 156:
+      return <Peliculas156 />
+    case 157:
+      return <Peliculas157 />
+    case 158:
+      return <Peliculas158 />
+    case 159:
+      return <Peliculas159 />
+    case 160:
+      return <Peliculas160 />
+    case 161:
+      return <Peliculas161 />
+    case 162:
+      return <Peliculas162 />
+    case 163:
+      return <Peliculas163 />
+    case 164:
+      return <Peliculas164 />
+    case 165:
+      return <Peliculas165 />
+    case 166:
+      return <Peliculas166 />
+    case 167:
+      return <Peliculas167 />
+    case 168:
+      return <Peliculas168 />
+    case 169:
+      return <Peliculas169 />
+    case 170:
+      return <Peliculas170 />
+    case 171:
+      return <Peliculas171 />
+    case 172:
+      return <Peliculas172 />
+    case 173:
+      return <Peliculas173 />
+    case 174:
+      return <Peliculas174 />
+    case 175:
+      return <Peliculas175 />
+    case 176:
+      return <Peliculas176 />
+    case 177:
+      return <Peliculas177 />
+    case 178:
+      return <Peliculas178 />
+    case 179:
+      return <Peliculas179 />
+    case 180:
+      return <Peliculas180 />
+    case 181:
+      return <Peliculas181 />
+    case 182:
+      return <Peliculas182 />
+    case 183:
+      return <Peliculas183 />
+    case 184:
+      return <Peliculas184 />
     default:
-      return <Peliculas1 />
+      return <Peliculas184 />
   }
 }
 
