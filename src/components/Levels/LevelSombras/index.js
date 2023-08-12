6544e2f43a40b954cae34de1a5789eec7c9490e7
patricio.toledo <patricio.ezequiel.toledo@gmail.com>
2023-08-12 11:16:49 -0300

    Imagenes de sombras

--- src/components/Levels/LevelSombras/index.js
+++ src/components/Levels/LevelSombras/index.js
@@ -25,6 +25,80 @@ import {
   Sombras23,
   Sombras24,
   Sombras25,
+  Sombras26,
+  Sombras27,
+  Sombras28,
+  Sombras29,
+  Sombras30,
+  Sombras31,
+  Sombras32,
+  Sombras33,
+  Sombras34,
+  Sombras35,
+  Sombras36,
+  Sombras37,
+  Sombras38,
+  Sombras39,
+  Sombras40,
+  Sombras41,
+  Sombras42,
+  Sombras43,
+  Sombras44,
+  Sombras45,
+  Sombras46,
+  Sombras47,
+  Sombras48,
+  Sombras49,
+  Sombras50,
+  Sombras51,
+  Sombras52,
+  Sombras53,
+  Sombras54,
+  Sombras55,
+  Sombras56,
+  Sombras57,
+  Sombras58,
+  Sombras59,
+  Sombras60,
+  Sombras61,
+  Sombras62,
+  Sombras63,
+  Sombras64,
+  Sombras65,
+  Sombras66,
+  Sombras67,
+  Sombras68,
+  Sombras69,
+  Sombras70,
+  Sombras71,
+  Sombras72,
+  Sombras73,
+  Sombras74,
+  Sombras75,
+  Sombras76,
+  Sombras77,
+  Sombras78,
+  Sombras79,
+  Sombras80,
+  Sombras81,
+  Sombras82,
+  Sombras83,
+  Sombras84,
+  Sombras85,
+  Sombras86,
+  Sombras87,
+  Sombras88,
+  Sombras89,
+  Sombras90,
+  Sombras91,
+  Sombras92,
+  Sombras93,
+  Sombras94,
+  Sombras95,
+  Sombras96,
+  Sombras97,
+  Sombras98,
+  Sombras99,
 } from '../../SVG/Sombras'
 
 const LevelSombras = ({ level }) => {
@@ -79,8 +153,156 @@ const LevelSombras = ({ level }) => {
       return <Sombras24 />
     case 25:
       return <Sombras25 />
+    case 26:
+      return <Sombras26 />
+    case 27:
+      return <Sombras27 />
+    case 28:
+      return <Sombras28 />
+    case 29:
+      return <Sombras29 />
+    case 30:
+      return <Sombras30 />
+    case 31:
+      return <Sombras31 />
+    case 32:
+      return <Sombras32 />
+    case 33:
+      return <Sombras33 />
+    case 34:
+      return <Sombras34 />
+    case 35:
+      return <Sombras35 />
+    case 36:
+      return <Sombras36 />
+    case 37:
+      return <Sombras37 />
+    case 38:
+      return <Sombras38 />
+    case 39:
+      return <Sombras39 />
+    case 40:
+      return <Sombras40 />
+    case 41:
+      return <Sombras41 />
+    case 42:
+      return <Sombras42 />
+    case 43:
+      return <Sombras43 />
+    case 44:
+      return <Sombras44 />
+    case 45:
+      return <Sombras45 />
+    case 46:
+      return <Sombras46 />
+    case 47:
+      return <Sombras47 />
+    case 48:
+      return <Sombras48 />
+    case 49:
+      return <Sombras49 />
+    case 50:
+      return <Sombras50 />
+    case 51:
+      return <Sombras51 />
+    case 52:
+      return <Sombras52 />
+    case 53:
+      return <Sombras53 />
+    case 54:
+      return <Sombras54 />
+    case 55:
+      return <Sombras55 />
+    case 56:
+      return <Sombras56 />
+    case 57:
+      return <Sombras57 />
+    case 58:
+      return <Sombras58 />
+    case 59:
+      return <Sombras59 />
+    case 60:
+      return <Sombras60 />
+    case 61:
+      return <Sombras61 />
+    case 62:
+      return <Sombras62 />
+    case 63:
+      return <Sombras63 />
+    case 64:
+      return <Sombras64 />
+    case 65:
+      return <Sombras65 />
+    case 66:
+      return <Sombras66 />
+    case 67:
+      return <Sombras67 />
+    case 68:
+      return <Sombras68 />
+    case 69:
+      return <Sombras69 />
+    case 70:
+      return <Sombras70 />
+    case 71:
+      return <Sombras71 />
+    case 72:
+      return <Sombras72 />
+    case 73:
+      return <Sombras73 />
+    case 74:
+      return <Sombras74 />
+    case 75:
+      return <Sombras75 />
+    case 76:
+      return <Sombras76 />
+    case 77:
+      return <Sombras77 />
+    case 78:
+      return <Sombras78 />
+    case 79:
+      return <Sombras79 />
+    case 80:
+      return <Sombras80 />
+    case 81:
+      return <Sombras81 />
+    case 82:
+      return <Sombras82 />
+    case 83:
+      return <Sombras83 />
+    case 84:
+      return <Sombras84 />
+    case 85:
+      return <Sombras85 />
+    case 86:
+      return <Sombras86 />
+    case 87:
+      return <Sombras87 />
+    case 88:
+      return <Sombras88 />
+    case 89:
+      return <Sombras89 />
+    case 90:
+      return <Sombras90 />
+    case 91:
+      return <Sombras91 />
+    case 92:
+      return <Sombras92 />
+    case 93:
+      return <Sombras93 />
+    case 94:
+      return <Sombras94 />
+    case 95:
+      return <Sombras95 />
+    case 96:
+      return <Sombras96 />
+    case 97:
+      return <Sombras97 />
+    case 98:
+      return <Sombras98 />
+    case 99:
+      return <Sombras99 />
     default:
-      return <Sombras1 />
+      return <Sombras99 />
   }
 }
 
--- src/components/SVG/Sombras/index.js
+++ src/components/SVG/Sombras/index.js
@@ -23,6 +23,80 @@ import Sombras22 from './sombras22'
 import Sombras23 from './sombras23'
 import Sombras24 from './sombras24'
 import Sombras25 from './sombras25'
+import Sombras26 from './sombras26'
+import Sombras27 from './sombras27'
+import Sombras28 from './sombras28'
+import Sombras29 from './sombras29'
+import Sombras30 from './sombras30'
+import Sombras31 from './sombras31'
+import Sombras32 from './sombras32'
+import Sombras33 from './sombras33'
+import Sombras34 from './sombras34'
+import Sombras35 from './sombras35'
+import Sombras36 from './sombras36'
+import Sombras37 from './sombras37'
+import Sombras38 from './sombras38'
+import Sombras39 from './sombras39'
+import Sombras40 from './sombras40'
+import Sombras41 from './sombras41'
+import Sombras42 from './sombras42'
+import Sombras43 from './sombras43'
+import Sombras44 from './sombras44'
+import Sombras45 from './sombras45'
+import Sombras46 from './sombras46'
+import Sombras47 from './sombras47'
+import Sombras48 from './sombras48'
+import Sombras49 from './sombras49'
+import Sombras50 from './sombras50'
+import Sombras51 from './sombras51'
+import Sombras52 from './sombras52'
+import Sombras53 from './sombras53'
+import Sombras54 from './sombras54'
+import Sombras55 from './sombras55'
+import Sombras56 from './sombras56'
+import Sombras57 from './sombras57'
+import Sombras58 from './sombras58'
+import Sombras59 from './sombras59'
+import Sombras60 from './sombras60'
+import Sombras61 from './sombras61'
+import Sombras62 from './sombras62'
+import Sombras63 from './sombras63'
+import Sombras64 from './sombras64'
+import Sombras65 from './sombras65'
+import Sombras66 from './sombras66'
+import Sombras67 from './sombras67'
+import Sombras68 from './sombras68'
+import Sombras69 from './sombras69'
+import Sombras70 from './sombras70'
+import Sombras71 from './sombras71'
+import Sombras72 from './sombras72'
+import Sombras73 from './sombras73'
+import Sombras74 from './sombras74'
+import Sombras75 from './sombras75'
+import Sombras76 from './sombras76'
+import Sombras77 from './sombras77'
+import Sombras78 from './sombras78'
+import Sombras79 from './sombras79'
+import Sombras80 from './sombras80'
+import Sombras81 from './sombras81'
+import Sombras82 from './sombras82'
+import Sombras83 from './sombras83'
+import Sombras84 from './sombras84'
+import Sombras85 from './sombras85'
+import Sombras86 from './sombras86'
+import Sombras87 from './sombras87'
+import Sombras88 from './sombras88'
+import Sombras89 from './sombras89'
+import Sombras90 from './sombras90'
+import Sombras91 from './sombras91'
+import Sombras92 from './sombras92'
+import Sombras93 from './sombras93'
+import Sombras94 from './sombras94'
+import Sombras95 from './sombras95'
+import Sombras96 from './sombras96'
+import Sombras97 from './sombras97'
+import Sombras98 from './sombras98'
+import Sombras99 from './sombras99'
 
 export {
   Sombras1,
@@ -50,4 +124,78 @@ export {
   Sombras23,
   Sombras24,
   Sombras25,
+  Sombras26,
+  Sombras27,
+  Sombras28,
+  Sombras29,
+  Sombras30,
+  Sombras31,
+  Sombras32,
+  Sombras33,
+  Sombras34,
+  Sombras35,
+  Sombras36,
+  Sombras37,
+  Sombras38,
+  Sombras39,
+  Sombras40,
+  Sombras41,
+  Sombras42,
+  Sombras43,
+  Sombras44,
+  Sombras45,
+  Sombras46,
+  Sombras47,
+  Sombras48,
+  Sombras49,
+  Sombras50,
+  Sombras51,
+  Sombras52,
+  Sombras53,
+  Sombras54,
+  Sombras55,
+  Sombras56,
+  Sombras57,
+  Sombras58,
+  Sombras59,
+  Sombras60,
+  Sombras61,
+  Sombras62,
+  Sombras63,
+  Sombras64,
+  Sombras65,
+  Sombras66,
+  Sombras67,
+  Sombras68,
+  Sombras69,
+  Sombras70,
+  Sombras71,
+  Sombras72,
+  Sombras73,
+  Sombras74,
+  Sombras75,
+  Sombras76,
+  Sombras77,
+  Sombras78,
+  Sombras79,
+  Sombras80,
+  Sombras81,
+  Sombras82,
+  Sombras83,
+  Sombras84,
+  Sombras85,
+  Sombras86,
+  Sombras87,
+  Sombras88,
+  Sombras89,
+  Sombras90,
+  Sombras91,
+  Sombras92,
+  Sombras93,
+  Sombras94,
+  Sombras95,
+  Sombras96,
+  Sombras97,
+  Sombras98,
+  Sombras99,
 }
