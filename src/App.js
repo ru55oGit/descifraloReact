8436372a015768909283f6216a80cb8e5575efc0
patricio.toledo <patricio.ezequiel.toledo@gmail.com>
2025-02-17 15:18:12 -0300

    ¨Posthog¨

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+import { PostHogProvider } from 'posthog-js/react'
 import { GameProvider } from './store/game'
 import { LanguageProvider } from './store/language'
 import IntroPage from './pages/introPage'
@@ -13,22 +15,27 @@ import theme from './theme'
 function App() {
   return (
     <StyledEngineProvider injectFirst>
-      <LanguageProvider>
-        <GameProvider>
-          <ThemeProvider theme={theme}>
-            <Router>
-              <Routes>
-                <Route element={<IntroPage />} path="/" />
-                <Route element={<CategoriesPage />} path="/categorias" />
-                <Route element={<PlayPage />} path="/jugar" />
-                <Route element={<LevelsPage />} path="/niveles" />
-                <Route element={<SuccessPage />} path="/sig/:category" />
-                <Route element={<StatisticsPage />} path="/estadisticas" />
-              </Routes>
-            </Router>
-          </ThemeProvider>
-        </GameProvider>
-      </LanguageProvider>
+      <PostHogProvider
+        apiKey="phc_f2aPtg549cBhRcXslGO03BMRaj6ZMYrUEfAl7w8UluR"
+        options={{ api_host: 'https://us.i.posthog.com' }}
+      >
+        <LanguageProvider>
+          <GameProvider>
+            <ThemeProvider theme={theme}>
+              <Router>
+                <Routes>
+                  <Route element={<IntroPage />} path="/" />
+                  <Route element={<CategoriesPage />} path="/categorias" />
+                  <Route element={<PlayPage />} path="/jugar" />
+                  <Route element={<LevelsPage />} path="/niveles" />
+                  <Route element={<SuccessPage />} path="/sig/:category" />
+                  <Route element={<StatisticsPage />} path="/estadisticas" />
+                </Routes>
+              </Router>
+            </ThemeProvider>
+          </GameProvider>
+        </LanguageProvider>
+      </PostHogProvider>
     </StyledEngineProvider>
   )
 }
